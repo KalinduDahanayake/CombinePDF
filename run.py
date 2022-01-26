a1a3117e63d627927285178d14f485902d0c51bb
KalinduDahanayake <kalindu.dahanayake2@gmail.com>
2022-01-26 14:53:29 +1100

    Better file search

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+from msilib.schema import File
 import os
 from PyPDF2 import PdfFileMerger, PdfFileReader
  
@@ -8,10 +9,10 @@ os.chdir('C:/Users/Kalindu/Desktop/CombinePDF/FilestoCombine')
 
 path, dirs, files = next(os.walk('C:/Users/Kalindu/Desktop/CombinePDF/FilestoCombine'))
 
-# Loop through all of them and append their pages
-for fileNumber in range(1, len(files)+1):
-    mergedObject.append(PdfFileReader('Combine_' + str(fileNumber)+ '.pdf', 'rb'))
- 
+for filename in os.listdir('.'):
+       if filename.endswith('.pdf'):
+           mergedObject.append(filename)
+
 # Write all the files into a file which is named as shown below
 os.chdir('C:/Users/Kalindu/Desktop/CombinePDF')
 mergedObject.write("CombinedPDF.pdf")
